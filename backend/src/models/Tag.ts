56398c161bb1eac2b0c1b6aac186daf633c146e0
Laurence-LaurencE <laurence20gros@gmail.com>
2025-11-07 18:08:01 +0100

    model Tag complet

---
+++ backend/src/models/Tag.ts
@@ -0,0 +1,25 @@
+export interface TagsRow {
+    id_tag: number | null;
+    tag_name: string;
+}
+
+export class Tags {
+    protected id_tag: number | null;
+    protected tag_name: string;
+
+    constructor(
+        id_tag: number | null,
+        tag_name: string,
+    ) {
+        this.id_tag = id_tag;
+        this.tag_name = tag_name;
+    }
+
+    getIdTag = () => {
+        return this.id_tag;
+    };
+
+    getTagName = () => {
+        return this.tag_name;
+    };
+}
